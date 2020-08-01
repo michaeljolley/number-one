dfa812c2e085ea0e02c9dfb18d279dda95167bd9
Michael Jolley <mike@sparcapp.io>
2020-08-01 15:57:35 -0500

    Adding sound effect command

---
+++ src/chat/commands/_soundEffect.ts
@@ -0,0 +1,28 @@
+import fs from 'fs'
+import { OnCommandEvent, OnSayEvent, OnSoundEffectEvent } from "../../models"
+import { EventBus, Events } from "../../events"
+
+/**
+ * Determines if the command is an audio clip and attempts to play if so
+ * @param onCommandEvent 
+ */
+export function _SoundEffect(onCommandEvent: OnCommandEvent) {
+
+  const cooldownSeconds = 60
+
+  // The broadcaster & mods are allowed to bypass throttling. Otherwise,
+  // only proceed if the command hasn't been used within the cooldown.
+  if (onCommandEvent.flags.broadcaster ||
+    onCommandEvent.flags.mod ||
+    onCommandEvent.extra.sinceLastCommand.any < cooldownSeconds * 1000) {
+    return
+  }
+
+  const filename = `${onCommandEvent.command.toLocaleLowerCase()}.mp3`
+  const fullpath = `/overlays/wwwroot/assets/audio/clips/${filename}`
+
+  if (fs.existsSync(filename)) {
+    // Send a the sfx to Socket.io
+    EventBus.eventEmitter.emit(Events.OnSoundEffect, new OnSoundEffectEvent(filename))
+  }
+}
