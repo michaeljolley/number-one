fcc5ded63c9b74fff2183521ba155b774a571162
Anthony Conrad (parithon) <parithon@outlook.com>
2020-10-13 19:20:55 -0700

    Fixed index.htm generated assets src (#52)

---
+++ src/web/admin/vue.config.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const path = require('path')
+
+// https://cli.vuejs.org/config/#vue-config-js
+module.exports = {
+  // https://cli.vuejs.org/config/#publicpath
+  publicPath: '/admin',
+  // https://cli.vuejs.org/config/#devserver-proxy
+  // devServer: {
+  //   proxy: {
+  //     '/': {
+  //       target: "<url>",
+  //       ws: true,
+  //       changeOrigin: true
+  //     }
+  //   }
+  // }
+}
