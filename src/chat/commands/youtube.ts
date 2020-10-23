c5e898e69184cbc740b4f7e07fd81161877c7834
Joakim Waltersson <Joakim.waltersson@gmail.com>
2020-10-23 19:21:06 +0200

    Fix copy-pasta comment and update Youtube chat message

--- src/chat/commands/youtube.ts
+++ src/chat/commands/youtube.ts
@@ -3,7 +3,7 @@ import { EventBus, Events } from "../../events"
 import { ShouldThrottle } from '../shouldThrottle'
 
 /**
- * Sends a message to chat with details about Michael's Twitter
+ * Sends a message to chat with details about Michael's Youtube
  * @param onCommandEvent 
  */
 export function Youtube(onCommandEvent: OnCommandEvent): void {
@@ -17,7 +17,7 @@ export function Youtube(onCommandEvent: OnCommandEvent): void {
     return
   }
 
-  const message = `You can catch all our streams on YouTube at https://www.youtube.com/baldbeardedbuilder`
+  const message = `For more content, check out the Bald Bearded Builder on YouTube at https://www.youtube.com/baldbeardedbuilder`
 
   // Send the message to Twitch chat
   EventBus.eventEmitter.emit(Events.OnSay, new OnSayEvent(message))
