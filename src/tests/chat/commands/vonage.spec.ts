e258b01228d917d834be15698796d9142f6ce92a
Michael Jolley <mike@sparcapp.io>
2020-11-04 16:27:43 -0600

    Adding Vonage command

---
+++ src/tests/chat/commands/vonage.spec.ts
@@ -0,0 +1,65 @@
+import { expect } from 'chai'
+import sinon from 'sinon'
+import 'mocha'
+
+import { Vonage } from '../../../chat/commands/vonage'
+import { OnCommandEvent } from '../../../models'
+import { EventBus, Events } from '../../../events'
+
+import { activeStream, onCommandExtra, user, viewerFlags } from '../../test-objects'
+
+let onCommandEvent: OnCommandEvent
+
+beforeEach(() => {
+  onCommandEvent = new OnCommandEvent(
+    user(),
+    'vonage',
+    '!vonage',
+    viewerFlags(),
+    onCommandExtra(),
+    activeStream())
+})
+
+afterEach(() => {
+  EventBus.eventEmitter.removeAllListeners()
+})
+
+describe('Commands: Vonage', () => {
+
+  it('should send message to chat', () => {
+    const spy = sinon.spy()
+
+    const emitter = EventBus.eventEmitter
+    emitter.on(Events.OnSay, spy)
+
+    Vonage(onCommandEvent)
+
+    expect(spy.called).to.equal(true)
+  })
+
+  it('should not send events if on cooldown', () => {
+    const spy = sinon.spy()
+
+    const emitter = EventBus.eventEmitter
+    emitter.on(Events.OnSay, spy)
+
+    onCommandEvent.extra.sinceLastCommand.any = 10
+
+    Vonage(onCommandEvent)
+
+    expect(spy.called).to.equal(false)
+  })
+
+  it('should not send events if on user cooldown', () => {
+    const spy = sinon.spy()
+
+    const emitter = EventBus.eventEmitter
+    emitter.on(Events.OnSay, spy)
+
+    onCommandEvent.extra.sinceLastCommand.user = 10
+
+    Vonage(onCommandEvent)
+
+    expect(spy.called).to.equal(false)
+  })
+})
